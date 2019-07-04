8b3984cdb53f729944c65d6d7da369255ba17125
ghaff <mudashiruagm@gmail.com>
2019-07-04 18:26:56 -0400

    hero

--- config/database.php
+++ config/database.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Support\Str;
 
-return [
 
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
+
+return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
